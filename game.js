044d7b33b30994762c91c2c4af58c8ecf3dadbf8
marcialino <marcialino100@gmail.com>
2023-06-09 17:13:58 -0300

    criei super_mario_game

---
+++ game.js
@@ -0,0 +1,11 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale:2,
+    clearcolor:[0,0,0,1]
+})
+
+loadRoot("https://i.imgur.com/") /*url para puxar as imagens/sprite*/
+
+loadSprite('bloco', '')
+/*url da imagens: Imagens: https://imgur.com/a/F8Jkryq*/ 
